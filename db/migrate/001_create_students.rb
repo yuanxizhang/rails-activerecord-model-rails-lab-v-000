8b54d2c25cfa3d708b3894e2e247cb906d47dcd8
Yancy Zhang <yuanxi.zhang@gmail.com>
2019-03-05 18:15:14 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -1,8 +1,8 @@
 class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
-      t.string :title
-      t.text :description
+      t.string :first_name
+      t.string :last_name
  
       t.timestamps null: false
     end
